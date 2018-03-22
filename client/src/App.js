b5ad8d7a18cdf0829b5bb63989185cd42eff203e
Juliette Rapala <jrapala@gmail.com>
2018-03-21 20:44:22 -0500

    Adding router.

--- client/src/App.js
+++ client/src/App.js
@@ -1,38 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Container } from './components/Grid';
+import Home from './pages/Home';
 
-class App extends Component {
-  state = {
-    response: ''
-  };
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-  componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
-  }
-
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
-
-    if (response.status !== 200) throw Error(body.message);
-
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.response}</p>
-      </div>
-    );
-  }
-}
+const App = () => (
+	<Container>
+		<Router>
+			<Switch>
+				<Route path="/" component={Home} />
+			</Switch>
+		</Router>
+	</Container>
+);
 
 export default App;
---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './home.css';
+
+class Home extends Component {
+  state = {
+    response: ''
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/hello');
+    const body = await response.json();
+
+    if (response.status !== 200) throw Error(body.message);
+
+    return body;
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.response}</p>
+      </div>
+    );
+  }
+}
+
+export default Home;
