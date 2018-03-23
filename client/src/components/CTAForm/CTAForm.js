a4e112ca9d6c6298cffc42dfaefe25db214c9719
Juliette Rapala <jrapala@gmail.com>
2018-03-22 20:48:51 -0500

    Adding nonfunctional form.

--- client/src/components/CTAForm/CTAForm.js
+++ client/src/components/CTAForm/CTAForm.js
@@ -2,8 +2,15 @@ import React from 'react';
 import './CTAForm.css';
 
 const CTAForm = props => (
-	<div className="form-wrapper">
-		<button className="subscribe-btn">Subscribe</button>
+	<div>
+		<form id="CTA-form">
+			<div id="CTA-email-wrapper">
+				<input id="CTA-email" type="email" name="email" placeholder="you@yourdomain.com" />
+			</div>
+			<div id="CTA-button-wrapper">
+				<input type="submit" value="Sign Up" id="CTA-button" />
+			</div>
+		</form>
 	</div>
 );
 
