bf5f341023096e439ba985ff99baa04efd32ff9a
Howard Kier <howard@Howards-MacBook-Air.local>
2018-04-09 15:41:30 -0500

    react-flexboxgrid added

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Grid, Row, Col } from 'react-flexboxgrid'
 import CTAForm from '../../components/CTAForm';
 import './home.css';
 import axios from 'axios';
