c45513344bc09f2bd0b14a04581f044a8011eff9
Juliette Rapala <jrapala@gmail.com>
2018-04-10 18:29:08 -0500

    Creating components in the learn page.

--- client/src/components/WhyDoula/WhyDoula.js
+++ client/src/components/WhyDoula/WhyDoula.js
@@ -3,17 +3,15 @@ import './WhyDoula.css';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 
 const WhyDoula = props => (
-	<Row center="xs" className="doula-reasons">
-		<Col xs={12} sm={4} left="xs">
-			<img src={props.logo} />
-                </Col>
-                <Col xs={12} sm={8} right="xs">
-                  <h3>props.reason</h3>
-                  <p>props.explanation</p>
-                </Col>
-       </Row>
-
-
+    <Row center="xs" className="doula-reasons">
+        <Col xs={12} sm={4} left="xs">
+            <img src={props.section.logo} />
+        </Col>
+        <Col xs={12} sm={8} right="xs">
+            <h3>{props.section.reason}</h3>
+            <p>{props.section.explanation}</p>
+        </Col>
+    </Row>
 );
 
 export default WhyDoula;
--- client/src/pages/Learn/Learn.js
+++ client/src/pages/Learn/Learn.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import './learn.css';
 import axios from 'axios';
-import why_one from "../../assets/img/learnDoula/one.png";
-import why_two from "../../assets/img/learnDoula/two.png";
-import why_three from "../../assets/img/learnDoula/three.png";
-import why_four from "../../assets/img/learnDoula/four.png";
+import why_one from '../../assets/img/learnDoula/one.png';
+import why_two from '../../assets/img/learnDoula/two.png';
+import why_three from '../../assets/img/learnDoula/three.png';
+import why_four from '../../assets/img/learnDoula/four.png';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import WhyDoula from '../../components/WhyDoula';
 
+const info = {
+  sections: [
+    {
+      key: '01',
+      title: 'What is a Doula?',
+      logo: './../assets/img/learnDoula/one.png',
+      reason: 'Parental Support',
+      explanation: 'A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.'
+    }
+  ]
+};
+
 class Learn extends Component {
-
-
   render() {
     return (
       <div className="learn-page">
@@ -22,10 +32,17 @@ class Learn extends Component {
                 <h1>What is a Doula?</h1>
               </Row>
               <Row center="xs">
-                <p>A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.</p>
+                <p>
+                  A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.
+                </p>
               </Row>
+
+              <div>
+                {info.sections.map(section => <WhyDoula section={section} key={section.key} />)}
+              </div>
+
               <Row center="xs" className="doula-reasons">
-                <Col xs={12} sm={4} left="xs">
+                <Col xs={12} sm={4}>
                   <img src={why_one} />
                 </Col>
                 <Col xs={12} sm={8} right="xs">
@@ -63,19 +80,17 @@ class Learn extends Component {
                   <p>Doulas are trained to assist families and provide informational after the baby is born.</p>
                 </Col>
               </Row>
-            <Row center="xs" center="xs">
+              <Row center="xs" center="xs">
                 <button class="button">Find a Doula</button>
-            </Row>
+              </Row>
 
             </Col>
-
 
           </Row>
 
         </div>
       </div>
-      );
-    }
+    );
   }
-
-  export default Learn;
+}
+export default Learn;
