8c1ee89ba88ddeb5e420289e42717993fa8f062c
Juliette Rapala <jrapala@gmail.com>
2018-04-11 21:21:53 -0500

    Adding /comingsoon route.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Home from './pages/Home';
 import UserSurvey from './pages/UserSurvey';
 import Learn from './pages/Learn';
+import ComingSoon from './pages/Delivering';
 import './assets/react-toolbox/theme.css';
 import theme from './assets/react-toolbox/theme.js';
 import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
@@ -9,17 +10,18 @@ import ThemeProvider from 'react-toolbox/lib/ThemeProvider';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => (
-	<ThemeProvider theme={theme}>
-		<div className="App">
-			<Router>
-				<Switch>
-					<Route path="/search" component={UserSurvey} />
-					<Route path="/learn" component={Learn} />
-					<Route path="/" component={Home} />
-				</Switch>
-			</Router>
-		</div>
-	</ThemeProvider>
+    <ThemeProvider theme={theme}>
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/search" component={UserSurvey} />
+                    <Route path="/learn" component={Learn} />
+                    <Route path="/comingsoon" component={ComingSoon} />
+                    <Route path="/" component={Home} />
+                </Switch>
+            </Router>
+        </div>
+    </ThemeProvider>
 );
 
 export default App;
--- client/src/pages/Delivering/Delivering.js
+++ client/src/pages/Delivering/Delivering.js
@@ -2,23 +2,13 @@ import React, { Component } from 'react';
 import { Row } from 'react-flexbox-grid';
 import './delivering.css';
 
-class Deliviering extends Component {
-
-
-
-
-
-
- 
-
-
-
+class Delivering extends Component {
   render() {
     return (
       <div className="delivering-page">
-      <Row center="xs">
-      <h1> We're working to deliver addition user experience! </h1>
-      </Row>
+        <Row center="xs">
+          <h1> We're working to deliver addition user experience! </h1>
+        </Row>
         <Row center="xs">
           <div>
 
