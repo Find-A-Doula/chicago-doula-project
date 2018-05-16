f7ceb0a6a846fb646ff149b5b3cb593c7b6d7e04
itsabhinaya <theabhinaya@gmail.com>
2018-05-15 20:24:39 -0500

    learn page css

--- client/src/pages/Learn/Learn.js
+++ client/src/pages/Learn/Learn.js
@@ -11,25 +11,25 @@ const info = {
       key: '01',
       logo: 'one',
       reason: 'Parental Support',
-      explanation: 'A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.'
+      explanation: 'In the weeks leading up to the birth, doulas are there to listen to the expecting mother\'s hopes and fears, help her create a birth plan, and empower her to make the best choices for her and her family.'
     },
     {
       key: '02',
       logo: 'two',
       reason: 'POSTPARTUM SUPPORT',
-      explanation: 'A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.'
+      explanation: 'Although doulas are not medical professionals, they do play an important role in the birth process  providing comfort measures such as massage, guided breathing, and recommending various positions for pain relief. The doula is there for physical and emotional support  during every type of birth.'
     },
     {
       key: '03',
       logo: 'three',
       reason: 'EMOTIONAL SUPPORT',
-      explanation: 'A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.'
+      explanation: 'After the baby arrives, doulas help families adapt to the stresses and joys of having a newborn. Doulas provide breastfeeding support, infant care advice and an extra hand around the house allowing the family to spend more time with their newborn.'
     },
     {
       key: '04',
       logo: 'four',
       reason: 'MUCH MORE',
-      explanation: 'A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.'
+      explanation: 'The doula\'s role goes beyond birth. There are doulas that support families through the adoption process, miscarriages, abortions, and other life experiences.'
     }
   ]
 };
@@ -64,32 +64,42 @@ class Learn extends Component {
             </div>
         </div>
 
-        <div className="doula">
-          <Row>
-            <Col xs={12}>
-              <Row center="xs">
-                <h1>What is a Doula?</h1>
-              </Row>
-              <Row className="doula-header" center="xs">
-                <p>
-                  A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.
-                </p>
-              </Row>
+        <div className="learn-page">
+          <div className="doula">
+            <Row>
+              <Col xs={12}>
 
-              <div>
-                {info.sections.map(section => <WhyDoula section={section} key={section.key} />)}
-              </div>
-              <Row center="xs">
-                <Link to="/comingsoon">
-                  <StdBtn value="Find a Doula"/>
-                </Link>
-              </Row>
+                <Row center="xs">
+                  <h1>What is a Doula?</h1>
+                </Row>
 
-            </Col>
+                <Row className="doula-intro" center="xs">
+                  <p>
+                    A professional trained in childbirth who provides constant physical and emotional support to families before, during and after pregnancy to help them achieve most satisfying birth experience possible.
+                  </p>
+                </Row>
+                <Row className="doula-desc" center="xs">
+                  <div>
+                    {info.sections.map(section => <WhyDoula section={section} key={section.key} />)}
+                  </div>
+                </Row>
 
-          </Row>
+                  <Row className="doula-header" center="xs">
+                    <p> Some doulas focus in one area while others have experience in various roles. </p>
+                  </Row>
 
-        </div>
+                  <Row center="xs">
+                    <Link to="/comingsoon">
+                      <StdBtn value="Find a Doula"/>
+                    </Link>
+                  </Row>
+
+              </Col>
+
+            </Row>
+
+          </div>
+      </div>
       </div>
     );
   }
